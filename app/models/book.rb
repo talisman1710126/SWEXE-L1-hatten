dadc3b13fc440f47826f4e1fc2e7fa25067a5146
廣地 一鷹 <2411140060r@ed.fuk.kindai.ac.jp>
2026-09-24 12:59:01 +0000

    yorozoh

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,8 @@
 class Book < ApplicationRecord
+  validates :title, presence: true
+  validates :author, presence: true
+  validates :published_year, presence: true
+  validates :published_year,
+            numericality: { only_integer: true, greater_than: 0 },
+            allow_nil: true
 end
